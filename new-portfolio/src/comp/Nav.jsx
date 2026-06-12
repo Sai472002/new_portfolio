65b4a4722af219726b1cb9cf6e4bf2e9337fda1b
Sai472002 <saidhasun0407@gmail.com>
2026-06-12 18:28:51 +0530

    nav started

---
+++ new-portfolio/src/comp/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <div>
+            <p>SB/</p>
+        </div>
+        <div>
+        
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
